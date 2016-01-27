88cfb302ffcacc77589f3fb64f1ad9020afd9556
santer51 <sether418@hotmail.com>
2016-01-26 19:17:33 -0700

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -1,5 +1,5 @@
 #if !defined(__cplusplus)
-#include <stdbool.h> /* C doesn't have booleans by default. */
+#include <stdbool.h> /* C doesn't have booleans by default. */nigga
 #endif
 #include <stddef.h>
 #include <stdint.h>
